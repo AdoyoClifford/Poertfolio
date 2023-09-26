a4a8e667c8c9c133b0024cec0d9a75e2aa9c8d80
Adoyo Clifford <adoyoclifford@gmail.com>
2023-09-26 23:33:26 +0300

    initial commit

---
+++ site/src/jsMain/kotlin/com/adoyo/sections/mainSection.kt
@@ -0,0 +1,33 @@
+package com.adoyo.sections
+
+import androidx.compose.runtime.Composable
+import com.adoyo.util.Constants.MAX_WIDTH
+import com.adoyo.util.Res
+import com.varabyte.kobweb.compose.css.ObjectFit
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.objectFit
+import com.varabyte.kobweb.silk.components.graphics.Image
+import org.jetbrains.compose.web.css.px
+
+@Composable
+fun MainSection() {
+    Box(
+        modifier = Modifier.maxWidth(MAX_WIDTH.px),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        MainBackground()
+    }
+}
+
+@Composable
+fun MainBackground() {
+    Image(
+        src = Res.Image.background,
+        desc = "background image",
+        modifier = Modifier.fillMaxSize().objectFit(ObjectFit.Cover)
+    )
+}
---
+++ site/src/jsMain/kotlin/com/adoyo/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.adoyo.util
+
+object Constants {
+    const val MAX_WIDTH = 1920
+}
+
+object Res {
+    object Image {
+        const val background = "background.svg"
+    }
+}
